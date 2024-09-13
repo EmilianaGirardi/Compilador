fcfc16bb6e08ac51bc75fef80d7f056c182174ca
Emiliana Girardi <girardiemiliana@gmail.com>
2024-09-13 18:26:58 -0300

    Se implementan contructor, getters y setters de la clase Par

--- src/AnalizadorLexico/Par.java
+++ src/AnalizadorLexico/Par.java
@@ -3,7 +3,25 @@ package AnalizadorLexico;
 import AnalizadorLexico.AccionesSemanticas.AccionSemantica;
 
 public class Par {
+
+
     private int estado;
     private AccionSemantica as;
 
+    //
+    public void setEstado(int estado) {
+        this.estado = estado;
+    }
+
+    public void setAs(AccionSemantica as) {
+        this.as = as;
+    }
+
+    public AccionSemantica getAs() {
+        return as;
+    }
+
+    public int getEstado() {
+        return estado;
+    }
 }
