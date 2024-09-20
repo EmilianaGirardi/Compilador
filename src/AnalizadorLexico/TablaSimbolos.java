c3b0b4cd7b7eb25c6d2e39ae4d66c5f5088fed7c
Nahuel7978 <roman.n7978@gmail.com>
2024-09-20 14:03:17 -0300

    Se crea un constructor en la tabla de simbolos el cual inicializa un HashMap y le carga los valores predefinidos de las palabras reservadas. A su vez se agrega un metodo que devuelve el valor correspondiente a una clave.

--- src/AnalizadorLexico/TablaSimbolos.java
+++ src/AnalizadorLexico/TablaSimbolos.java
@@ -4,19 +4,44 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class TablaSimbolos {
-    private Map<String, Integer> map = new HashMap<>();
-    //Map(Lexema, array de atributos )
-    //La tabla de simbolos guarda tokens de mas de un caracter y palabras reservadas
-    //el puntero a la tabla de simbolos es el string clave de acceso (lexema)
-    //los signos +, -, etc. no se guardan
+    private Map<String, Integer> map; 
+    
+    public TablaSimbolos() {
+		this.map =  new HashMap<>();
+		this.agregarPalabras();
+	}
+    
+    
+    private void agregarPalabras() {
+		// TODO Auto-generated method stub
+    	
+    	map.put("REPEAT",265);
+    	map.put("IF",266);
+    	map.put("THEN",267);
+    	map.put("ELSE",268);
+    	map.put("BEGIN",269);
+    	map.put("END",270);
+    	map.put("END_IF",271);
+    	map.put("OUTF",272);
+    	map.put("TYPEDEF",273);
+    	map.put("FUN",274);
+    	map.put("RET",275);
+    	map.put("GOTO",276);
+    	map.put("TRIPLE",277);
+    	
+	}
 
-    public void agregarToken(String lexema, Integer atributo){
+	public void agregarToken(String lexema, Integer atributo){
         map.put(lexema, atributo);
     }
 
-    public boolean estaToken(String lexema){
+	public boolean estaToken(String lexema){
         return map.containsKey(lexema);
     }
+	
+	public Integer getValor(String lexema) {
+		return map.get(lexema);
+	}
 
 }
 
