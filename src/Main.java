0d24167200abf903395bc3e92af6bd540d802453
Nahuel7978 <roman.n7978@gmail.com>
2024-09-30 14:27:02 -0300

    ...

--- src/Main.java
+++ src/Main.java
@@ -8,17 +8,10 @@ import AnalizadorLexico.TablaSimbolos;
 
 public class Main {
 	
-	//TODO cambiar que agregue el numero de token cada vez que agrega a la TS (DONE)
-	// Agregar al mapeo caracteres invalidos (DONE)
-	// verificar que devuelve el reed al final del archivo (DONE pero solucionar tema del EOF)
-	// AS nico
-	// modificar tamaño de la matriz y arreglar que detecte estados y acciones nuevas. (DONE)
-	
-	
     public static void main(String[] args) {
     	
     	String archivoPrueba="/home/roman7978/Documentos/Workspace_Compilador/Compilador/src/AnalizadorLexico/ArchivoPrueba/Prueba0";
-        
+
 		try {
 			Lexico l = new Lexico(archivoPrueba);
 			int v = 1;
